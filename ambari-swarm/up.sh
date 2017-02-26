c53fc58c35e25508b033943c7e2fc0e24fc23721
David W. Streever <david@streever.com>
2017-02-26 12:32:07 -0500

    no message

--- ambari-swarm/up.sh
+++ ambari-swarm/up.sh
@@ -6,4 +6,4 @@
 docker network create --driver=overlay --subnet 10.0.10.0/24 --attachable core
 
 # Deploy the stack
-docker stack deploy ambari-1 --compose-file=./docker-compose.yaml
+docker stack deploy ambari-1 --publish 8080:8080 --compose-file=./docker-compose.yaml
