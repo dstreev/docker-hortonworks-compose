fac5834097ace94df8141db04f0463802d657bc1
David W. Streever <david@streever.com>
2017-04-10 14:35:33 -0400

    Building out scenarios for Docker on YARN according to SLIDER-780

---
+++ docker-on-yarn/examples/scenario-1.1/start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+slider create example1_1 --template appConfig.json --metainfo metainfo.json --resources resources.json
---
+++ hdp-distributed-cluster/pull_images.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+pdsh -g dk docker -H localhost:2375 pull dstreev/centos7_ambari:2
